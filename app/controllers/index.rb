aa9cc0f7e791934063fb9fc1da25bd5c4c220243
Jay C <imjching@hotmail.com>
2015-08-26 13:03:45 +0800

    Add registration, sign in, sign out, sessions and user validation functions.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,58 @@
+#login page
 get '/' do
   # Look in app/views/index.erb
   erb :index
+end
+
+#form for signups
+get '/sign_in' do
+  #if successful
+  erb :sign_in
+end
+
+post '/sign_in' do
+  @email = params[:email]
+  @password = params[:password]
+
+  if User.authenticate(@email, @password)
+    user = User.find_by email: @email
+    session[:user_id] = user.id
+    redirect to '/secret'
+  else
+    redirect to '/'
+  end
+end
+
+get '/register' do
+  erb :register
+end
+
+post '/register' do
+  p session[:email]
+  byebug
+
+  @name = params[:name]
+  @email = params[:email]
+  @password = params[:password]
+
+  if User.check_duplicate(@email) == false
+    redirect to '/'
+  else
+    @user = User.create(name: @name, email: @email, password: @password)
+    erb :secret
+  end
+end
+
+get '/secret' do
+  if session[:user_id]
+    @user = User.find(session[:user_id])
+    erb :secret
+  else
+    redirect '/sign_in'
+  end
+end
+
+post '/logout' do
+  session[:user_id] = nil
+  redirect to '/'
 end
---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ActiveRecord::Base
+  # Remember to create a migration!
+
+  def self.check_duplicate(email)
+    temp = self.find_by email: email
+
+    if temp == nil
+      true
+    else
+      false
+    end
+  end
+
+  def self.authenticate(email, password)
+    check = self.find_by email: email, password: password
+    if check == nil
+      false
+    else
+      true
+    end
+  end
+end
